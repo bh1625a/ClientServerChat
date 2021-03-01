b1d66e35af355315b9145eadbeaf6ff8c9017d93
Benjamin <benj@minclifford.co.uk>
2021-03-01 12:55:37 +0000

    Add test cases to ServerTest

---
+++ test/main/ServerTest.java
@@ -0,0 +1,23 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @Test
+    void getIP() {
+    }
+
+    @Test
+    void setIP() {
+    }
+
+    @Test
+    void getPort() {
+    }
+
+    @Test
+    void setPort() {
+    }
+}
