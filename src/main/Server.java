b3ef4008e5edb480bde5a568ab17b86907d04c2f
Benjamin <benj@minclifford.co.uk>
2021-03-01 13:22:43 +0000

    Add test cases to ServerTest

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,31 +1,27 @@
 package main;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Handler;
 
 public class Server {
-    private String IP;
-    private int port;
 
+    // All client names, so we can check for duplicates upon registration.
+    private static Set<String> names = new HashSet<>();
 
-    public Server(String IP, int port){
-        this.IP = IP;
-        this.port = port;
+    // The set of all the print writers for all the clients, used for broadcast.
+    private static Set<PrintWriter> writers = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("The chat server is running...");
+        ExecutorService pool = Executors.newFixedThreadPool(500);
+        try (ServerSocket listener = new ServerSocket(59001)) {
+            while (true) {
+                pool.execute(new Handler(listener.accept()));
+            }
+        }
     }
-
-    public String getIP() {
-        return IP;
-    }
-
-    public void setIP(String IP) {
-        this.IP = IP;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-}
