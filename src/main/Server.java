bec7e5aa69a4844ccb88cc353cca500f6e5b2029
maninandra <mn1453e@gre.ac.uk>
2021-03-01 12:46:47 +0000

    Server Getters and Setters

--- src/main/Server.java
+++ src/main/Server.java
@@ -1,6 +1,29 @@
 package main;
 
 public class Server {
-    // Server class
+    private String IP;
+    private int port;
+
+    public Server(String IP, int port){
+        this.IP = IP;
+        this.port = port;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
 
 }
